ca298549f6470f9bf11a7ca13dabcf50e2c15d0b
Sam Buchmiller <buc11003@byui.edu>
2013-11-11 14:46:46 -0700

    Created a class to store information about what shape a ray intersects with.

---
+++ basic-raytracer/Ray Tracer/src/scene/ISect.java
@@ -0,0 +1,28 @@
+package scene;
+
+import math.Vector3;
+import raytracer.Ray;
+
+public class ISect
+{
+   private Shape shape;
+   private Ray ray;
+   private float distance;
+
+   public ISect(Shape shape, Ray ray, float distance)
+   {
+      this.shape = shape;
+      this.ray = ray;
+      this.distance = distance;
+   }
+
+   public Vector3 getPosition()
+   {
+      return shape.getPosition();
+   }
+
+   public float getIntensity()
+   {
+      throw new RuntimeException("Not yet implemented");
+   }
+}
