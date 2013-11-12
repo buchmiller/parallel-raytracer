49582a26709a242707e7d28f73dfb7dc91f48d3a
Sam Buchmiller <buc11003@byui.edu>
2013-11-12 12:30:07 -0700

    Added more advanced camera features.

--- basic-raytracer/Ray Tracer/src/scene/Camera.java
+++ basic-raytracer/Ray Tracer/src/scene/Camera.java
@@ -6,11 +6,25 @@ public class Camera
 {
    private Vector3 position;
    private Vector3 direction;
+   private float nearClippingPlane;
+   private float farClippingPlane;
+   private float fov;
+   private float angle;
 
    public Camera(Vector3 position, Vector3 direction)
    {
+      this(position, direction, 90f, 0.1f, 1000f);
+   }
+
+   public Camera(Vector3 position, Vector3 direction, float fov,
+                 float nearClippingPlane, float farClippingPlane)
+   {
       this.position = position;
       this.direction = direction;
+      this.fov = fov;
+      this.angle = (float) Math.atan(Math.toRadians(fov * 0.5));
+      this.nearClippingPlane = nearClippingPlane;
+      this.farClippingPlane = farClippingPlane;
    }
 
    public Vector3 getPosition()
@@ -21,5 +35,25 @@ public class Camera
    public Vector3 getDirection()
    {
       return direction;
+   }
+
+   public float getNearClippingPlane()
+   {
+      return nearClippingPlane;
+   }
+
+   public float getFarClippingPlane()
+   {
+      return farClippingPlane;
+   }
+
+   public float getFov()
+   {
+      return fov;
+   }
+
+   public float getAngle()
+   {
+      return angle;
    }
 }
