7b309c05915152a3989116a278691855dfb9a246
Sam Buchmiller <buc11003@byui.edu>
2013-11-13 16:42:32 -0700

    Added a min and max to each ray.

--- basic-raytracer/Ray Tracer/src/raytracer/Ray.java
+++ basic-raytracer/Ray Tracer/src/raytracer/Ray.java
@@ -6,11 +6,23 @@ public class Ray
 {
    private Vector3 origin;
    private Vector3 direction;
+   private float min;
+   private float max;
 
-   public Ray(Vector3 origin, Vector3 direction)
+   public Ray(Vector3 origin, Vector3 direction, float min, float max)
    {
       this.origin = origin;
       this.direction = direction;
+      this.min = min;
+      this.max = max;
+   }
+
+   public Ray(Vector3 src, Vector3 dest)
+   {
+      this.origin = new Vector3(src);
+      this.direction = new Vector3(dest.getX() - src.getX(),
+            dest.getY() - src.getY(), dest.getZ() - src.getZ());
+      this.direction.normalize();
    }
 
    public Vector3 getOrigin()
@@ -23,4 +35,13 @@ public class Ray
       return direction;
    }
 
+   public float getMin()
+   {
+      return min;
+   }
+
+   public float getMax()
+   {
+      return max;
+   }
 }
