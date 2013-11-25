bc75b4a2a388fefe64f9ff6808480cb05b264157
Sam Buchmiller <buc11003@byui.edu>
2013-11-25 14:41:42 -0700

    Created a test client.

--- basic-parallel/NetworkedThreads/src/concurrency/MyClient.java
+++ basic-parallel/NetworkedThreads/src/concurrency/MyClient.java
@@ -1,6 +1,37 @@
 package concurrency;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class MyClient
 {
+   public MyClient()
+   {
+      String serverName = "localhost";
+      int port = 3000;
+      
+      try
+      {
+         System.out.println("Connecting to " + serverName + " on port " + port);
+         Socket client = new Socket(serverName, port);
+         System.out.println("Now connected to " + client.getRemoteSocketAddress());
+         OutputStream outToServer = client.getOutputStream();
+         DataOutputStream out = new DataOutputStream(outToServer);
 
+         out.writeUTF("Hello from " + client.getLocalSocketAddress());
+
+         InputStream inFromServer = client.getInputStream();
+         DataInputStream in = new DataInputStream(inFromServer);
+         System.out.println("Server says " + in.readUTF());
+         client.close();
+      }
+      catch (IOException e)
+      {
+         System.out.println("Error: " + e);
+      }
+   }
 }
