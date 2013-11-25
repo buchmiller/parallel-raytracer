536ceadb7a998f9f3e49c2f2953bef2e7a79deb1
Sam Buchmiller <buc11003@byui.edu>
2013-11-25 14:45:40 -0700

    Created a test server.

--- basic-parallel/NetworkedThreads/src/concurrency/MyServer.java
+++ basic-parallel/NetworkedThreads/src/concurrency/MyServer.java
@@ -1,18 +1,62 @@
 package concurrency;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MyServer
+public class MyServer extends Thread
 {
+   private ServerSocket serverSocket;
    private Socket client;
    private int poolSize = 12;
 
-   public void start()
+   public MyServer(int port) throws IOException
+   {
+      serverSocket = new ServerSocket(port);
+   }
+
+   @Override
+   public void run()
+   {
+      while (true)
+      {
+         try
+         {
+            System.out.println("Server started with address: " + InetAddress.getLocalHost());
+            System.out.println("Waiting for client on port "
+                  + serverSocket.getLocalPort() + "...");
+            Socket server = serverSocket.accept();
+            System.out.println("Just connected to " + server.getRemoteSocketAddress());
+
+            DataInputStream in = new DataInputStream(server.getInputStream());
+            System.out.println(in.readUTF());
+
+            DataOutputStream out = new DataOutputStream(server.getOutputStream());
+
+            out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
+            server.close();
+         }
+         catch (SocketTimeoutException e)
+         {
+            System.out.println("Socket timed out!\n" + e);
+         }
+         catch (IOException e)
+         {
+            System.out.println("Error: " + e);
+         }
+      }
+   }
+
+   public void begin()
    {
       ExecutorService executorService = Executors.newFixedThreadPool(2);
       CompletionService<ResultData> completionService =
