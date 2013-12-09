8eb035f990a15d340c8cc69191949893b8a36d9f
Sam Buchmiller <buc11003@byui.edu>
2013-12-09 16:54:40 -0700

    Reduced Thread.sleep to be insignificant for faster testing.

--- Ray Tracer/src/common/raytracer/TracerCallable.java
+++ Ray Tracer/src/common/raytracer/TracerCallable.java
@@ -27,7 +27,7 @@ public class TracerCallable implements Callable<ResultData>
       //This sleep is only for testing, remove once more advance ray tracing is added
       try
       {
-         Thread.sleep(100);
+         Thread.sleep(10);
       }
       catch (InterruptedException e)
       {
