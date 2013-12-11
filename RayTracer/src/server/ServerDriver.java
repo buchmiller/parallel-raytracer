f4ad9a8cd867cfacf8cc40794c8617adfa53c781
Sam Buchmiller <buc11003@byui.edu>
2013-12-11 10:44:37 -0700

    ServerDriver now requires the port number as a parameter.

--- RayTracer/src/server/ServerDriver.java
+++ RayTracer/src/server/ServerDriver.java
@@ -6,10 +6,21 @@ public class ServerDriver
 {
    public static void main(String[] args)
    {
+      if (args.length < 1)
+      {
+         System.out.println("Must enter port number");
+         return;
+      }
+
       try
       {
-         Thread t = new Server(3000);
+         int port = Integer.parseInt(args[0]);
+         Thread t = new Server(port);
          t.start();
+      }
+      catch (NumberFormatException e)
+      {
+         System.out.println("Error: port must be an integer.\n" + e);
       }
       catch (IOException e)
       {
