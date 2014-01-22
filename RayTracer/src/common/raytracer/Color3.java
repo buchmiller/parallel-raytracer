b2eadfa9ef4bf5b61caea1fa79b78cbf08bc5898
Sam Buchmiller <buc11003@byui.edu>
2014-01-21 21:23:48 -0700

    Made black equal 0.

--- RayTracer/src/common/raytracer/Color3.java
+++ RayTracer/src/common/raytracer/Color3.java
@@ -6,7 +6,7 @@ public class Color3 implements Serializable
 {
    private Vector3 vector;
    public static final Color3 WHITE = new Color3(200, 200, 200);
-   public static final Color3 BLACK = new Color3(10, 10, 10);
+   public static final Color3 BLACK = new Color3(0, 0, 0);
    public static final Color3 RED = new Color3(100, 0, 0);
    public static final Color3 GREEN = new Color3(0, 100, 0);
    public static final Color3 BLUE = new Color3(0, 0, 100);
