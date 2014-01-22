bdebc03b38b820ac5a2903d3c1f9dc5bf37d4efe
Sam Buchmiller <buc11003@byui.edu>
2014-01-21 22:37:02 -0700

    Added a scene to test reflectivity.

--- RayTracer/src/client/Client.java
+++ RayTracer/src/client/Client.java
@@ -45,7 +45,8 @@ public class Client
       Color3 bColor = new Color3(0, 1, 0);
       int maxDepth = 4;
 
-      testScene = Scene.createShadowTest(camera, screen, bColor, maxDepth);
+      testScene = Scene.createReflectTest(camera, screen, bColor, maxDepth);
+//      testScene = Scene.createShadowTest(camera, screen, bColor, maxDepth);
 //      testScene = Scene.createLightTest(camera, screen, bColor, maxDepth);
 //      testScene = Scene.createSimple(camera, screen, bColor, maxDepth);
    }
